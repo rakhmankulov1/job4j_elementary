c6839e7f0527e9f2e393f821802aeef26eb78459
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2020-07-13 21:09:33 +0300

    3.3. Разбор первой программы. [#325666]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Damir Rakhmankulov");
+        System.out.println("13.07.2020");
+    }
+
+}
