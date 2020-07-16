47dc8c56138bb5da616576f63f966939844f3e07
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2020-07-16 22:30:45 +0300

    9.2. Checkstyle [#325661]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -11,6 +11,5 @@ public class ArgMethod {
 
         ArgMethod.hello(name);
 
-
     }
 }
--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(int p, int k) {
-        double rsl = ((p/(2*(k+1))*k))*(p/(2*(k+1)));
+        double rsl = ((p / (2 * (k + 1)) * k)) * (p / (2 * (k + 1)));
         return rsl;
     }
 
