f9d34a334f56025e60be64b6ea20968013be817b
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2020-08-08 20:47:22 +0300

    5.7. Ипотека [#325702]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 1;
+        percent = percent / 100;
+        double loan = (amount + (amount * percent)) - salary;
+        while (loan > 0) {
+            year++;
+            loan = (loan + (loan * percent))-salary;
+        }
+
+        return year;
+    }
+}
