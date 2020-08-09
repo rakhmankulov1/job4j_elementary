b86002b2714ee4c90cdae366c6348f114753e606
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2020-08-09 13:08:27 +0300

    6.1. Классический поиск перебором. [#325617]

---
+++ test/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] input = {5, 4, 3, 2};
+        int value = 5;
+        int result = FindLoop.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHas3Then0() {
+        int[] input = {5, 4, 3, 2};
+        int value = 3;
+        int result = FindLoop.indexOf(input, value);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHasNot7ThenNotPassed() {
+        int[] input = {5, 4, 3, 2};
+        int value = 7;
+        int result = FindLoop.indexOf(input, value);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHasNotSuchIndexThenNotPassed() {
+        int[] input = {5, 4, 3, 2};
+        int value = 5;
+        int result = FindLoop.indexOf(input, value);
+        int expect = 4;
+        assertThat(result, is(expect));
+    }
+}
