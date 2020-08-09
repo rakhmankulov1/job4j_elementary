eedddc64dfc66eacacc27e1434a47a25ab1f7e08
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2020-08-09 12:22:18 +0300

    6.0.2. Заполнение массива. [#325631]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Antony";
+        names[1] = "Mark";
+        names[2] = "Eddy";
+        names[3] = "Ryan";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
