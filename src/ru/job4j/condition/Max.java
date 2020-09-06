1197af3a38cc60c928e492b03cac04f4aeffbef3
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2020-09-06 20:59:05 +0300

    3. Перегрузить метод max для трех чисел [#325786]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -6,7 +6,20 @@ public class Max {
         return result;
     }
 
+    public static int max(int left, int right, int up) {
+        int tmp = max(right, up);
+        return max(left, tmp);
+    }
+
+    public static int max(int left, int right, int up, int down) {
+        int tmpFirst = max(left, right);
+        int tmpSecond = max(up, down);
+        return max(tmpFirst, tmpSecond);
+    }
+
     public static void main(String[] args) {
         System.out.println(Max.max(1, 2));
+        System.out.println(Max.max(1, 2, 3));
+        System.out.println(Max.max(1, 2, 3, 4));
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -24,4 +24,40 @@ public class MaxTest {
         int result = Max.max(3, 3);
         assertThat(result, is(3));
     }
+
+    @Test
+    public void whenMax1To2And3Then3() {
+        int result = Max.max(1, 2,3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMaxLeftToRightAndUpThen5() {
+        int result = Max.max(3, 2,5);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenBothEqual3Then3() {
+        int result = Max.max(3, 3,3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax1To2And3And4Then4() {
+        int result = Max.max(1, 2,3,4);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenMaxLeftToRightAndUpAndDownThen5() {
+        int result = Max.max(3, 2,5,4);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenBothEqual5Then5() {
+        int result = Max.max(5, 5,5,5);
+        assertThat(result, is(5));
+    }
 }
